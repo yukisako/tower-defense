fbdc5f9c214f093980a0694c6490d0dc75e6b186
yukisako <yukisakoatoz@gmail.com>
2017-03-31 13:37:23 +0900

    add unity ads

---
+++ Assets/Scripts/UnityAds.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAds : MonoBehaviour
+{
+	public void ShowAd()
+	{
+		if (Advertisement.IsReady())
+		{
+			var options = new ShowOptions { resultCallback = HandleShowResult };
+			Advertisement.Show( options);
+		}
+	}
+
+	private void HandleShowResult(ShowResult result)
+	{
+		switch (result)
+		{
+		case ShowResult.Finished:
+			Debug.Log("The ad was successfully shown.");
+			//Application.LoadLevel ("Main");
+			//
+			// YOUR CODE TO REWARD THE GAMER
+			// Give coins etc.
+			break;
+		case ShowResult.Skipped:
+			Debug.Log("The ad was skipped before reaching the end.");
+			break;
+		case ShowResult.Failed:
+			Debug.LogError("The ad failed to be shown.");
+			break;
+		}
+	}
+}
+
+
+/*
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAdsExample : MonoBehaviour
+{
+  public void ShowAd()
+  {
+    if (Advertisement.IsReady())
+    {
+      Advertisement.Show();
+    }
+  }
+}
+*/
