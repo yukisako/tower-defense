cb104f0e8a32d3b0a0df6f25d8857997a26c23a1
yukisako <yukisakoatoz@gmail.com>
2017-04-05 11:41:24 +0900

    bug fix

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -4,66 +4,39 @@ using UnityEngine;
 
 public class StartButton : MonoBehaviour {
 
-	private bool japanese = true; 
-
-
-
-
 	public void SceneLoad (){
 		Application.LoadLevel ("Main");
 	}
 
 	public void HowToPlay(){
-		MyCanvas.SetActive ("LanguageButton", true);
-		MyCanvas.SetActive ("ExplainImage", true);
-		MyCanvas.SetActive ("CloseButton", true);
 		MyCanvas.SetActive ("ExplainEnglishText", false);
 		MyCanvas.SetActive ("ExplainJapaneseText", true);
+		MyCanvas.SetActive ("ExplainImage", true);
+		MyCanvas.SetActive ("CloseButton", true);
+		MyCanvas.SetActive ("JapaneseButton", false);
+		MyCanvas.SetActive ("EnglishButton", true);
 	}
 
 	public void CloseButton(){
 		MyCanvas.SetActive ("ExplainImage", false);
 		MyCanvas.SetActive ("CloseButton", false);
-		MyCanvas.SetActive ("LanguageButton", false);
+		MyCanvas.SetActive ("JapaneseButton", false);
+		MyCanvas.SetActive ("EnglishButton", false);
 		MyCanvas.SetActive ("ExplainEnglishText", false);
 		MyCanvas.SetActive ("ExplainJapaneseText", false);
 	}
 
-	public void selectLangage(){
-		Debug.Log (japanese);
-		if (japanese == false) {
-			//日本語から英語へ切り替え
-			MyCanvas.SetActive ("ExplainEnglishText", false);
-			MyCanvas.SetActive ("ExplainJapaneseText", true);
-//			selectLangageButton.FormatLabel ("Japanese");
-			japanese = true;
-		} else {
-			Debug.Log (japanese);
-			MyCanvas.SetActive ("ExplainEnglishText", true);
-			MyCanvas.SetActive ("ExplainJapaneseText", false);
-//			selectLangageButton.FormatLabel ("English");
-			japanese = false;
-		}
+	public void selectJapanese(){
+		MyCanvas.SetActive ("ExplainEnglishText", false);
+		MyCanvas.SetActive ("ExplainJapaneseText", true);
+		MyCanvas.SetActive ("JapaneseButton", false);
+		MyCanvas.SetActive ("EnglishButton", true);
 	}
-//	private Canvas _canvas;
-//	void Start () {
-//		// Canvasコンポーネントを保持
-//		_canvas = GetComponent<Canvas>();
-//	}
-//
-//	/// 表示・非表示を設定する
-//	public void SetActive(string name, bool b) {
-//		foreach(Transform child in _canvas.transform) {
-//			// 子の要素をたどる
-//			if(child.name == name) {
-//				// 指定した名前と一致
-//				// 表示フラグを設定
-//				child.gameObject.SetActive(b);
-//				// おしまい
-//				return;
-//			}
-//		}
-//		// 指定したオブジェクト名が見つからなかった
-//		Debug.LogWarning("Not found objname:"+name);
-//	}
+
+	public void selectEnglish(){
+		MyCanvas.SetActive ("ExplainEnglishText", true);
+		MyCanvas.SetActive ("ExplainJapaneseText", false);
+		MyCanvas.SetActive ("JapaneseButton", true);
+		MyCanvas.SetActive ("EnglishButton", false);
+	}
 }
